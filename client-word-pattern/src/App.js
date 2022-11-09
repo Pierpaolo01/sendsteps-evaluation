d34bcd9443581e91ceca46a70c50744a2299291c
Pierpaolo Pascarella <Pierpaolo.pascarella@gmail.com>
2022-11-09 11:53:28 +0100

    Task: re-usable card component

--- client-word-pattern/src/App.js
+++ client-word-pattern/src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+
+import CardComponent from "./components/cardComponent/CardComponent";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-section">
+        <CardComponent title="Word patterns" description="Detect overlapping words within words">
+            
+        </CardComponent>
       </header>
     </div>
   );
