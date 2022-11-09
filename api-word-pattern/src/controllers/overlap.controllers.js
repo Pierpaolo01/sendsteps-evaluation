2d56bb09e39cc02c5d4602d77a2d6d175df06b18
Pierpaolo Pascarella <Pierpaolo.pascarella@gmail.com>
2022-11-09 20:46:54 +0100

    Task: business logic overlapping words

--- api-word-pattern/src/controllers/overlap.controllers.js
+++ api-word-pattern/src/controllers/overlap.controllers.js
@@ -4,10 +4,26 @@ const examineOverlap = (req, res) => {
     const {errors} = validationResult(req);
 
     if (errors.length) {
-        res.status(422).json(errors)
+        res.status(422).json(errors);
+        return;
     }
 
-    const {mainWord, overlapWord} = req.body;
+    const mainWordArray = req.body.mainWord.split('');
+    const overlapWordArray = req.body.overlapWord.split('');
+
+    let overlapResult = [];
+
+    for (let letter of overlapWordArray) {
+        mainWordArray.forEach((l) => {
+            if (l === letter) {
+                overlapResult.push(l);
+                mainWordArray.shift();
+            }
+        });
+    }
+
+    res.status(200)
+        .json(Array.from(new Set(overlapResult)));
 }
 
 module.exports = {
